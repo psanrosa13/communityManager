f6b3043208c19ff5329f70db605b717408845877
paula <psanrosa13@gmail.com>
2020-01-19 11:18:18 -0300

    primeiro commit

---
+++ src/main/java/com/community/manager/communityManager/CommunityManagerApplication.java
@@ -0,0 +1,13 @@
+package com.community.manager.communityManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/community/manager/communityManager/CommunityManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.manager.communityManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
